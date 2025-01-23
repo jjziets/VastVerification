1843809d00e6c3663dfcb6bf66b570c22559b9b3
Etherion <19214485+jjziets@users.noreply.github.com>
2025-01-23 10:56:10 +0200

    Update build_vastai.sh

--- build_vastai.sh
+++ build_vastai.sh
@@ -1,2 +1,17 @@
+#!/usr/bin/env bash
+#
+# build_and_push.sh
+#
+# Purpose: Compile GPUburn Docker image, extract binaries, build the test image,
+#          and push it to the registry.
+set -e  # Exit script immediately on any error
+echo ">>> Moving into gpu-burn folder..."
+cd build_gpuburn/gpu-burn/
+echo ">>> Building gpu_burn Docker image and extracting binaries..."
+./build_gpu_burn.sh
+echo ">>> Copying gpu_burn and compare.ptx back to main folder..."
+cp gpu_burn ../../
+cp compare.ptx ../../
+cd ../../
 docker build -t vastai/test:selftest .
 docker push vastai/test:selftest
